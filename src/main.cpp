3cfca3c1e89b98ee494ce18720bafe3495dce7b1
T. Vandenryt <thijs.vandenryt@uhasselt.be>
2022-04-06 15:32:25 +0200

    Added credentials template

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 #include <WiFi.h>
 #include "Credentials.h"
 
+  #include <esp_wpa2.h> //wpa2 library for connections to Enterprise networks
+    const char *ssid = WPA2_SSID;
+    const char *password = WPA2_PASSWORD;
+    const char *identity = WPA2_EAP_IDENTITY;
+
 void setup() {
   // put your setup code here, to run once:
 }
