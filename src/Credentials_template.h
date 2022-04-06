dacbcd6dcf2940dae836104a4a8416c9d7def9f9
T. Vandenryt <24638205+vanThijs@users.noreply.github.com>
2022-04-06 15:58:47 +0200

    Update Credentials_template.h

--- src/Credentials_template.h
+++ src/Credentials_template.h
@@ -1,4 +1,4 @@
-//This file will contain the credentials, fill out and rename to Credentials.h
+//This file will contain the credentials, fill out and rename to "Credentials.h"
   //WPA2-Config
   #define WPA2_SSID "eduroam" // eduroam SSID
   #define WPA2_PASSWORD "XXXXXXXXXXXXXXXXXXXXXXXX" //Eduroam password
