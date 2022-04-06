b97892c6c14798b6404c722618aeed1296236f02
T. Vandenryt <thijs.vandenryt@uhasselt.be>
2022-04-06 15:32:38 +0200

    Added Credentials template

---
+++ src/Credentials_template.h
@@ -0,0 +1,5 @@
+//This file will contain the credentials, fill out and rename to Credentials.h
+  //WPA2-Config
+  #define WPA2_SSID "eduroam" // eduroam SSID
+  #define WPA2_PASSWORD "XXXXXXXXXXXXXXXXXXXXXXXX" //Eduroam password
+  #define WPA2_EAP_IDENTITY "lucgXXXX@uhasselt.be" //lucg or lucp 
